58d607f55237355c995344a5b8a213d64a3d44fc
Alberto Soriano <albertosorino.e@gmail.com>
2023-07-02 21:45:58 -0500

    Agregando serializador a Json

---
+++ tareas/serializer.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Tarea, Usuario
+
+class TareaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tarea
+        fields = '__all__'
+
+class UsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuario
+        fields = '__all__'
