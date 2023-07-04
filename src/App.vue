695a408806a23b3735ef5fccdc99235c9cf00601
AndreaRodriguez08 <106575386+AndreaRodriguez08@users.noreply.github.com>
2023-07-04 08:45:26 -0500

    Actualización App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,47 +6,37 @@ export default {
   components: {
     Sidebar,
     DiaSemana
-  },
-  data() {
+  }, data() {
     return {
       diasSemana: ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'],
-      nombreTarea: '',
-      desTarea: '', // Agrega esta línea
-      diaTarea: 0,
-      tareaTerminada: false, // Agrega esta línea
+      diaTarea: '',
+      nombreTarea: ''
     };
   },
   methods: {
-    actualizarNombreTarea(nuevaTarea) {
+    updateDiaSemana(dia) {
+      this.diaTarea = dia;
+    },
+    updateNombreTarea(nombre) {
+      this.nombreTarea = nombre;
+    },
+    nuevaTarea(nuevaTarea) {
       this.nombreTarea = nuevaTarea.texto;
+      this.diaTarea = nuevaTarea.diaTarea;
     },
-    actualizarDatos(datos) {
-      // Actualizar los datos en el componente padre
-      this.nombreTarea = datos.nombreTarea;
-      this.desTarea = datos.desTarea;
-      this.diaTarea = datos.diaSemana;
-      this.tareaTerminada = datos.tareaTerminada;
+    tareaSeleccionada(tarea){
+      this.nombreTarea = tarea.texto;
+      this.diaTarea = tarea.diaTarea;
+    }
+  }
+}
 
-      // Mostrar los datos en la consola
-      console.log('Nombre de tarea:', this.nombreTarea);
-      console.log('Descripción:', this.desTarea);
-      console.log('Día de la semana:', this.diaTarea);
-      console.log('Tarea terminada:', this.tareaTerminada);
-
-      // Limpiar los campos en el componente padre
-      // this.nombreTarea = '';
-      // this.desTarea = '';
-      // this.diaTarea = 0;
-      // this.tareaTerminada = false;
-    },
-  },
-};
 </script>
 
 <template>
   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#"></a>
+      <a class="navbar-brand" href="/">Mis Tareas</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
         aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -64,13 +54,12 @@ export default {
 
   <main class="d-flex flex-nowrap">
     <div class="col-4">
-      <Sidebar :nombre-tarea="nombreTarea" :des-tarea="desTarea" :dia-semana="diaTarea" :tarea-terminada="tareaTerminada"
-        @guardar-cambios="actualizarDatos"></Sidebar>
+      <Sidebar :dia-semana="diaTarea" :nombre-tarea="nombreTarea"></Sidebar>
     </div>
     <div class="col-8 panelSemana d-flex flex-nowrap overflow-auto">
       <ul class="dias d-flex flex-row">
         <li v-for="dia in diasSemana" :key="dia">
-          <DiaSemana :nombre="dia" @tarea-creada="actualizarNombreTarea"></DiaSemana>
+          <DiaSemana :nombre="dia" @tarea-creada="nuevaTarea" @seleccion-tarea="tareaSeleccionada"></DiaSemana>
         </li>
       </ul>
     </div>
@@ -78,30 +67,13 @@ export default {
 </template>
 
 <style scoped>
-* {
-  box-sizing: border-box;
-  padding: 0;
-}
-
-nav {
-  padding: 10px 0;
-}
-
 body {
   text-align: center;
-  width: 100%;
-  height: 100%;
-}
-
-main {
-  height: 90vh;
-  margin-top: 4%;
 }
 
 .panelSemana {
   overflow-x: scroll;
   width: 800px;
-  height: 100%;
 }
 
 .panelSemana::-webkit-scrollbar {
@@ -109,11 +81,14 @@ main {
   display: auto;
 }
 
+main {
+  margin-top: 4%;
+}
+
 .dias {
   list-style-type: none;
   padding: 0;
   margin: 0;
-  height: 100%;
 }
 
 .dias li {
