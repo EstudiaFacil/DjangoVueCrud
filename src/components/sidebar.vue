5233e2cfaa38d09039d50cc5434d1613acbdda66
AndreaRodriguez08 <106575386+AndreaRodriguez08@users.noreply.github.com>
2023-07-04 08:44:18 -0500

    Actualización sidebar.vue

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -1,93 +1,104 @@
 <script>
 export default {
-  props: ['nombreTarea', 'desTarea', 'diaSemana', 'tareaTerminada'],
-  data() {
-    return {
-      diasSemana: ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'],
-    };
-  },
-  methods: {
-    guardarCambios() {
-      const datos = {
-        nombreTarea: this.nombreTarea,
-        desTarea: this.desTarea,
-        diaSemana: this.diaSemana,
-        tareaTerminada: this.tareaTerminada,
-      };
-      this.$emit('guardar-cambios', datos);
+    props: {
+        diaSemana: {
+            type: String,
+            required: true
+        },
+        nombreTarea: {
+            type: String,
+            required: true
+        }
     },
-  },
-};
+    setup(props) {
+        // setup() receives props as the first argument.
+        console.log(props.diaSemana,props.nombreTarea);
+    },
+    data() {
+        return {
+            descripcion: '',
+            tareaTerminada: false,
+            dia: ''
+        };
+    },
+    methods: {
+        guardarCambios() {
+            this.nombreTarea = '';
+            this.descripcion = '';
+            this.diaSemana = '';
+            this.tareaTerminada = false;
+        }
+    }
+}
 </script>
 
 <template>
-    <div class="sidebar d-flex flex-column flex-shrink-0 bg-body-tertiary">
-
-        <h1>Tarea</h1>
-
+    <div class="sidebar d-flex flex-column flex-shrink-0 bg-body-tertiary" style="width: 280px;">
         <form class="needs-validation" novalidate>
-            <label for="titulo" class="form-label">Título</label>
-            <input type="text" class="form-control" id="titulo" name="titulo" placeholder="Título de tarea"
-              :value="nombreTarea" @input="$emit('update:nombre-tarea', $event.target.value)" required>
+            <input type="text" class="form-control" id="firstName" placeholder="Nombre tarea" :value="nombreTarea" required>
 
             <div class="invalid-feedback">
                 Ingrese un nombre válido.
             </div>
 
-            <div class="col-md-12">
-                <label for="descripcion" class="form-label">Descripción</label>
-                <textarea class="form-control" placeholder="Descripción de tarea" id="descripcion" name="descripcion"
-                  style="height: 100px" @input="$emit('update:des-tarea', $event.target.value)"></textarea>
-
+            <div class="col-12">
+                <label for="lastName" class="form-label">Descripción</label>
+                <textarea class="form-control" placeholder="Descripción de tarea" id="floatingTextarea2"
+                    style="height: 100px"></textarea>
                 <div class="invalid-feedback">
-                    Una descripción es necesaria.
+                    Una descripciòn es necesaria.
                 </div>
             </div>
 
             <div class="col-md-12">
                 <label for="country" class="form-label">Día</label>
-                <select class="form-select" :value="diaSemana"
-                  @input="$emit('update:dia-semana', parseInt($event.target.value))" required>
-                    <option disabled selected value="0">Selecciona un día</option>
-                    <option value="1">Lunes</option>
-                    <option value="2">Martes</option>
-                    <option value="3">Miércoles</option>
-                    <option value="4">Jueves</option>
-                    <option value="5">Viernes</option>
-                    <option value="6">Sábado</option>
-                    <option value="7">Domingo</option>
+                <select class="form-select" :value=diaSemana required>
+                    <option value="Lunes">Lunes</option>
+                    <option value="Martes">Martes</option>
+                    <option value="Miércoles">Miércoles</option>
+                    <option value="Jueves">Jueves</option>
+                    <option value="Viernes">Viernes</option>
+                    <option value="Sábado">Sábado</option>
+                    <option value="Domingo">Domingo</option>
                 </select>
                 <div class="invalid-feedback">
                     Selecciona un dia.
                 </div>
             </div>
 
+
+
             <hr class="my-4">
 
             <div class="form-check">
-                <input type="checkbox" class="form-check-input" id="tarea-terminada" :checked="tareaTerminada"
-                  @change="$emit('update:tarea-terminada', $event.target.value)">
-                <label class="form-check-label" for="tarea-terminada">Tarea terminada</label>
+                <input type="checkbox" class="form-check-input" id="same-address">
+                <label class="form-check-label" for="same-address">Tarea terminada</label>
             </div>
 
             <hr class="my-4">
 
-            <button class="btn btn-primary btn-lg" @click.prevent="guardarCambios">Guardar cambios</button>
+            <button class="btn btn-primary" @click="guardarCambios">Guardar cambios</button>
+            <button class="btn btn-danger" @click="eliminarTarea">Eliminar</button>
 
         </form>
 
         <hr>
         <div class="dropdown">
             <a href="#" class="d-flex align-items-center link-body-emphasis text-decoration-none dropdown-toggle"
-              data-bs-toggle="dropdown" aria-expanded="false">
-                <img src="./icons/perfil-del-usuario.png" alt="" width="32" height="32" class="rounded-circle me-2">
+                data-bs-toggle="dropdown" aria-expanded="false">
+                <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
                 <strong>mdo</strong>
             </a>
             <ul class="dropdown-menu text-small shadow">
+                <li><a class="dropdown-item" href="#">New project...</a></li>
+                <li><a class="dropdown-item" href="#">Settings</a></li>
+                <li><a class="dropdown-item" href="#">Profile</a></li>
+                <li>
+                    <hr class="dropdown-divider">
+                </li>
                 <li><a class="dropdown-item" href="#">Sign out</a></li>
             </ul>
         </div>
-
     </div>
 </template>
 
@@ -96,21 +107,11 @@ body {
     text-align: center;
 }
 
-h1 {
-    font-size: 2em;
-    margin: 0 auto 8px;
-    padding: 0;
-}
-
-label {
-    display: block;
-    margin-top: 4px;
-}
-
 .sidebar {
     margin: 0;
     padding: 3%;
-    height: 100%;
-    width: 280px;
+}
+button{
+    margin-right: 5px;
 }
 </style>
