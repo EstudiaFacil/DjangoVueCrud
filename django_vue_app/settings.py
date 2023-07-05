26f1f3f090c838e886559efcdfe4fa6b22c1334e
Alberto Soriano <albertosorino.e@gmail.com>
2023-07-05 02:22:49 -0500

    Conectado a db supabase

--- django_vue_app/settings.py
+++ django_vue_app/settings.py
@@ -80,9 +80,18 @@ WSGI_APPLICATION = 'django_vue_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'HOST':'db.vrultqhlxefctjpeaesw.supabase.co',
+        'NAME':'postgres',
+        'USER':'postgres',
+        'PASSWORD':'vuedjango51423',
+        'PORT':'5432',
     }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
